89613e4ea94e212dd51e9eeb554c0180f7f4a503
bhimanshu1 <bhimanshu7274@gmail.com>
2026-07-08 19:01:11 +0530

    created functions to initialise and retrive from RAG database, wrote the logic in the router, to route the requests whether they are knowledge based or data based.

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
-ECHO is on.
+from rag import rag_database_initialisation
+from rag import answer_policy_question
+from router import route
+# from rag import initialize_rag
+
+vector_db = rag_database_initialisation()
+# print(
+#     answer_policy_question("Do you insure pets?", vector_db)
+# )
+
+
+# vector_db = initialize_rag()
+
+while True:
+
+    question = input("\n> ")
+
+    if question.lower() == "exit":
+        break
+
+    answer = route(question, vector_db)
+
+    print("\n", answer)
--- router.py
+++ router.py
@@ -1,1 +1,44 @@
-ECHO is on.
+import re
+from tools import get_order
+from rag import answer_policy_question
+
+
+def extract_order_id(question: str):
+    match = re.search(r"ORD\d+", question.upper())
+    if match:
+        return match.group()
+    return None
+
+def route(question, vector_db):
+    order_id = extract_order_id(question)
+    if order_id is None:
+        return answer_policy_question(question, vector_db)
+    
+    question = question.lower()
+    combined_keywords = [
+        "return",
+        "refund",
+        "replace",
+        "exchange",
+        "eligible"
+    ]
+
+    for word in combined_keywords:
+        if word in question:
+            order = get_order(order_id)
+            if order is None:
+                return "Invalid order ID."
+            
+            combined_question = f"""
+            Return policy for {order['category']}
+            Customer asked:
+            {question}
+            Status:
+            {order['status']}
+            """
+            return answer_policy_question(combined_question, vector_db)
+
+    order = get_order(order_id) 
+    if order == None:
+        return "Product Not Found" 
+    return order
